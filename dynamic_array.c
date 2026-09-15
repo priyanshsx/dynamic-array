d81467d385a508355662a1c4d8c2f4c879bc7a95
Priyansh <saxenapriyansh95@gmail.com>
2026-09-15 06:33:04 +0530

    drafted the initial structure of the program

---
+++ dynamic_array.c
@@ -0,0 +1,22 @@
+// step 1: create a pointer to hold the array with: 
+// an integer variable for capacity and one integer variable for size 
+
+
+
+// step 2: ask the os for a tiny block of memory using malloc 
+
+
+
+// step 3: start an input loop where the user continuously types numbers 
+
+
+
+// step 4: before adding the user_input number to the dynamic_array if size == capacity 
+
+
+
+// step 5: reallocation in memory based on the new size of the dynamic array 
+
+
+
+// step 6: cleanup 
